85b6a1a2e205091e2486bc46a3bf9b360bc6476a
ewan-r <ewan.rougier@laposte.net>
2022-09-30 10:56:19 +0200

    suite TD liste chainée progC

--- LinkedList/projet/Domain/List/List.c
+++ LinkedList/projet/Domain/List/List.c
@@ -30,3 +30,28 @@ int list_ins_next(List *list, ListElmt *element, const void *data){
   list->size++;
   return 0;
 }
+
+int list_rem_next(List *list, ListElmt *element, void **data){
+  ListElmt *old;
+  if (list->size==0)
+    return -1;
+
+  if(element == NULL){
+    old = list->head;
+    if(list->size == 0)
+      list->tail=NULL;
+    list->head=old->next;
+  }
+  else{
+    if(element == list->tail)
+      return -1;
+    old = element->next;
+    if(old == list->tail)
+      list->tail=element;
+    element->next=old->next;
+  }
+  *data = old->data;
+  free(old);
+  list->size --;
+  return 1;
+}
